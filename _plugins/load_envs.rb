94bfe3c42fceaed8f602717e58fb04e9a17a6acb
steveoh <sgourley@utah.gov>
2016-09-13 19:32:42 -0600

    is head what i'm after

--- _plugins/load_envs.rb
+++ _plugins/load_envs.rb
@@ -7,13 +7,11 @@ module Jekyll
   class EnvironmentVariablesGenerator < Generator
 
     def generate(site)
-      site.config['env'] = Dotenv.load
       site.config['REPOSITORY_URL'] = ENV['REPOSITORY_URL']
       site.config['BRANCH'] = ENV['BRANCH']
       site.config['PULL_REQUEST'] = ENV['PULL_REQUEST']
       site.config['HEAD'] = ENV['HEAD']
       site.config['CONTEXT'] = ENV['CONTEXT']
-      site.config['JEKYLL_ENV'] = ENV['JEKYLL_ENV']
       site.config['canary'] = 'tweet tweet.'
       # Add other environment variables to `site.config` here...
     end
