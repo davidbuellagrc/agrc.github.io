b7ed4861e58bbf0c5de821eb5e080e6cfa5bffa8
steveoh <sgourley@utah.gov>
2016-09-13 19:40:05 -0600

    remove dotenv and compare head to branch

--- _plugins/load_envs.rb
+++ _plugins/load_envs.rb
@@ -7,6 +7,7 @@ module Jekyll
   class EnvironmentVariablesGenerator < Generator
 
     def generate(site)
+      site.config['BRANCH'] = ENV['BRANCH']
       site.config['HEAD'] = ENV['HEAD']
       site.config['canary'] = 'tweet tweet.'
       # Add other environment variables to `site.config` here...
