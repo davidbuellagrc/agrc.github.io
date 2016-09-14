b7de8e35f0da384809363e519ae5214b990836cf
steveoh <sgourley@utah.gov>
2016-09-13 18:55:59 -0600

    play with envs

---
+++ _plugins/load_envs.rb
@@ -0,0 +1,18 @@
+# Plugin to add environment variables to the `site` object in Liquid templates
+
+module Jekyll
+
+  class EnvironmentVariablesGenerator < Generator
+
+    def generate(site)
+      site.config['REPOSITORY_URL'] = ENV['REPOSITORY_URL']
+      site.config['BRANCH'] = ENV['BRANCH']
+      site.config['PULL_REQUEST'] = ENV['PULL_REQUEST']
+      site.config['HEAD'] = ENV['HEAD']
+      site.config['CONTEXT'] = ENV['CONTEXT']
+      # Add other environment variables to `site.config` here...
+    end
+
+  end
+
+end
