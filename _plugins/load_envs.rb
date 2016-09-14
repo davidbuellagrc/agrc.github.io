4a0027c223448b8c93a75283de97df75ad34031d
steveoh <sgourley@utah.gov>
2016-09-13 19:06:24 -0600

    try dotenv

--- _plugins/load_envs.rb
+++ _plugins/load_envs.rb
@@ -1,10 +1,13 @@
 # Plugin to add environment variables to the `site` object in Liquid templates
 
+require 'dotenv'
+
 module Jekyll
 
   class EnvironmentVariablesGenerator < Generator
 
     def generate(site)
+      site.config['env'] = Dotenv.load
       site.config['REPOSITORY_URL'] = ENV['REPOSITORY_URL']
       site.config['BRANCH'] = ENV['BRANCH']
       site.config['PULL_REQUEST'] = ENV['PULL_REQUEST']
