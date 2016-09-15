6fa2ea9c85b43ffbc2fb0d162c1e82a2b78afbda
steveoh <sgourley@utah.gov>
2016-09-15 13:43:22 -0600

    use new commit_ref env var

--- _plugins/load_envs.rb
+++ _plugins/load_envs.rb
@@ -5,10 +5,7 @@ module Jekyll
   class EnvironmentVariablesGenerator < Generator
 
     def generate(site)
-      site.config['BRANCH'] = ENV['BRANCH']
-      site.config['HEAD'] = ENV['HEAD']
-      site.config['canary'] = 'tweet tweet.'
-      # Add other environment variables to `site.config` here...
+      site.config['github']['build_revision'] = ENV['COMMIT_REF']
     end
 
   end
